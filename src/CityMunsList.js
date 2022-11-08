9455e8a226dd39b28e46a9821d98b0717322cd3f
Rowen Remis R. Iral <rowen_remis_iral@yahoo.com>
2022-11-08 08:55:53 +0800

    Updated citymunslist

--- src/CityMunsList.js
+++ src/CityMunsList.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect} from 'react';
 
 function CityMunsList(){
+    
     const [cityMuns, setCityMuns] = useState([]);
+
     useEffect(() =>{
         fetch('http://localhost:9000/citymuns')
         .then(response => response.json())
@@ -13,42 +15,47 @@ function CityMunsList(){
         });
     },[]);
 
-    //
-    let dropdown = document.getElementById('locality-dropdown');
-    dropdown.length = 0;
 
-    let defaultOption = document.createElement('option');
-    defaultOption.text = 'Choose State/Province';
-
-    dropdown.add(defaultOption);
-    dropdown.selectedIndex = 0;
 
     const url = 'http://localhost:9000/citymuns';
-    fetch(url)
-    .then(
-        (response) => {
-            if(response.status !== 200) { 
-                console.warn('Looks like there was a problem. Status Code: ' + 
-                response.status); 
-                return;
-            }
-            
-            // Examine the text in the response  
-            response.json().then(function(data) {  
-                let option;
-            
-                for (let i = 0; i < data.length; i++) {
-                option = document.createElement('option');
-                option.text = data[i].name;
-                option.value = data[i].abbreviation;
-                dropdown.add(option);
-                }    
-            });  
-        }
-    )
-    .catch((err)=>{
-        console.error('Fetch Error - ', err);
-    });
+    useEffect(()=>{
+        //
+        let dropdown = document.getElementById('locality-dropdown');
+        dropdown.length = 0;
+
+        let defaultOption = document.createElement('option');
+        defaultOption.text = 'Choose State/Province';
+
+        dropdown.add(defaultOption);
+        dropdown.selectedIndex = 0;
+
+        fetch(url)
+            .then(
+                (response) => {
+                    if(response.status !== 200) { 
+                        console.warn('Looks like there was a problem. Status Code: ' + 
+                        response.status); 
+                        return;
+                    }
+                    
+                    // Examine the text in the response  
+                    response.json().then(function(data) {  
+                        let option;
+                    
+                        for (let i = 0; i < data.length; i++) {
+                        option = document.createElement('option');
+                        option.text = data[i].name;
+                        option.value = data[i].abbreviation;
+                        dropdown.add(option);
+                        }    
+                    });  
+                }
+            )
+            .catch((err)=>{
+                console.error('Fetch Error - ', err);
+            });
+    },[]);
+    
     return (
         <div>
             <h1>List of City Municipalities</h1>
